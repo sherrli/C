b52c227ab152bf2d5ce86e437b4a04a75d632227
Sherri Li <32345806+shli17@users.noreply.github.com>
2019-06-03 17:45:44 -0700

    Create Graph.h

---
+++ Graph/Graph.h
@@ -0,0 +1,36 @@
+ /* Header file that specifies the operations that Graph.c exports */
+ 
+#include "List.h"
+#ifndef _GRAPH_H_INCLUDE_
+#define _LIST_H_INCLUDE_
+
+#define INF -1
+#define NIL 0
+
+//#include "List.h"
+
+/*** Exported reference type ***/
+typedef struct GraphObj* Graph;
+
+/*** Constructors-Destructors ***/
+Graph newGraph(int n);
+void freeGraph(Graph* pG);
+
+/*** Access functions ***/
+int getOrder(Graph G);
+int getSize(Graph G);
+int getSource(Graph G);
+int getParent(Graph G, int u);
+int getDist(Graph G, int u);
+void getPath(List L, Graph G, int u);
+
+/*** Manipulation procedures ***/
+void makeNull(Graph G);
+void addEdge(Graph G, int u, int v);
+void addArc(Graph G, int u, int v);
+void BFS(Graph G, int s);
+
+/*** Other operations ***/
+void printGraph(FILE* out, Graph G);
+
+#endif
